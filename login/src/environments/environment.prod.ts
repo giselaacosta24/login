613537af82fb695a135ff738077567a5c21c9ea6
giselaacosta24 <giseacosta651@gmail.com>
2022-04-01 00:35:26 -0300

    agrego firebase conexion

--- login/src/environments/environment.prod.ts
+++ login/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDdQGg0IeXAkj8HAnLSCO9hkn4CyEDJ1fc",
+    authDomain: "login-2168e.firebaseapp.com",
+    projectId: "login-2168e",
+    storageBucket: "login-2168e.appspot.com",
+    messagingSenderId: "499564081947",
+    appId: "1:499564081947:web:a1b6445d9d6e13d31999be",
+    measurementId: "G-SN1DZ8Q6XJ"
+  }
 };
